16063a6c94ad236b00e89c1819e10d7716638abb
nyx-desdev <nyx.desdev@gmail.com>
2024-09-22 15:50:09 +0530

    auth implementation

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
+import pickle
+from pathlib import Path
+
 import streamlit as st
+import streamlit_authenticator as stauth 
 from PyPDF2 import PdfReader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 import os
@@ -10,99 +14,126 @@ from langchain.chains.question_answering import load_qa_chain
 from langchain.prompts import PromptTemplate
 from dotenv import load_dotenv
 
-load_dotenv()
-os.getenv("GOOGLE_API_KEY")
-genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+st.set_page_config("Chat PDF")
+
+# --- USER AUTHENTICATION ---
+names = ["Nikhil Arya", "Vaibhav Gupta"]
+usernames = ["nyxdesdev", "vaibhu"] 
+
+# load hashed passwords
+file_path = Path(__file__).parent / "hashed_pw.pkl"
+with file_path.open("rb") as file:
+    hashed_passwords = pickle.load(file)
+
+authenticator = stauth.Authenticate(names, usernames, hashed_passwords,
+    "sales_dashboard", "abcdef", cookie_expiry_days=30)
+
+
+
+name, authentication_status, username = authenticator.login("Login", "main")
+
+if authentication_status == False:
+    st.error("Username/password is incorrect")
+
+if authentication_status == None:
+    st.warning("Please enter your username and password")
+
+if authentication_status:
+    load_dotenv()
+    os.getenv("GOOGLE_API_KEY")
+    genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
 
 
 
 
 
-def get_pdf_text(pdf_docs):
-    text=""
-    for pdf in pdf_docs:
-        pdf_reader= PdfReader(pdf)
-        for page in pdf_reader.pages:
-            text+= page.extract_text()
-    return  text
+    def get_pdf_text(pdf_docs):
+        text=""
+        for pdf in pdf_docs:
+            pdf_reader= PdfReader(pdf)
+            for page in pdf_reader.pages:
+                text+= page.extract_text()
+        return  text
 
 
 
-def get_text_chunks(text):
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=10000, chunk_overlap=1000)
-    chunks = text_splitter.split_text(text)
-    return chunks
+    def get_text_chunks(text):
+        text_splitter = RecursiveCharacterTextSplitter(chunk_size=10000, chunk_overlap=1000)
+        chunks = text_splitter.split_text(text)
+        return chunks
 
 
-def get_vector_store(text_chunks):
-    embeddings = GoogleGenerativeAIEmbeddings(model = "models/embedding-001")
-    vector_store = FAISS.from_texts(text_chunks, embedding=embeddings)
-    vector_store.save_local("faiss_index")
+    def get_vector_store(text_chunks):
+        embeddings = GoogleGenerativeAIEmbeddings(model = "models/embedding-001")
+        vector_store = FAISS.from_texts(text_chunks, embedding=embeddings)
+        vector_store.save_local("faiss_index")
 
 
-def get_conversational_chain():
+    def get_conversational_chain():
 
-    prompt_template = """
-    Answer the question as detailed as possible from the provided context, make sure to provide all the details, if the answer is not in
-    provided context just say, "answer is not available in the context", don't provide the wrong answer\n\n
-    Context:\n {context}?\n
-    Question: \n{question}\n
+        prompt_template = """
+        Answer the question as detailed as possible from the provided context, make sure to provide all the details, if the answer is not in
+        provided context just say, "answer is not available in the context", don't provide the wrong answer\n\n
+        Context:\n {context}?\n
+        Question: \n{question}\n
 
-    Answer:
-    """
+        Answer:
+        """
 
-    model = ChatGoogleGenerativeAI(model="gemini-pro",
-                             temperature=0.3)
+        model = ChatGoogleGenerativeAI(model="gemini-pro",
+                                temperature=0.3)
 
-    prompt = PromptTemplate(template = prompt_template, input_variables = ["context", "question"])
-    chain = load_qa_chain(model, chain_type="stuff", prompt=prompt)
+        prompt = PromptTemplate(template = prompt_template, input_variables = ["context", "question"])
+        chain = load_qa_chain(model, chain_type="stuff", prompt=prompt)
 
-    return chain
-
-
-
-def user_input(user_question):
-    embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
-    
-    # Enable dangerous deserialization (ONLY IF YOU TRUST THE SOURCE OF THE INDEX FILE)
-    new_db = FAISS.load_local("faiss_index", embeddings, allow_dangerous_deserialization=True) 
-
-    docs = new_db.similarity_search(user_question)
-
-    chain = get_conversational_chain()
-
-    response = chain(
-        {"input_documents": docs, "question": user_question},
-        return_only_outputs=True
-    )
-
-    print(response)
-    st.write("Reply: ", response["output_text"])
+        return chain
 
 
 
+    def user_input(user_question):
+        embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
+        
+        # Enable dangerous deserialization (ONLY IF YOU TRUST THE SOURCE OF THE INDEX FILE)
+        new_db = FAISS.load_local("faiss_index", embeddings, allow_dangerous_deserialization=True) 
 
-def main():
-    st.set_page_config("Chat PDF")
-    st.header("UPSC Research Assistant")
+        docs = new_db.similarity_search(user_question)
 
-    user_question = st.text_input("Ask a Question about disaster management")
+        chain = get_conversational_chain()
 
-    if user_question:
-        user_input(user_question)
+        response = chain(
+            {"input_documents": docs, "question": user_question},
+            return_only_outputs=True
+        )
 
-    # with st.sidebar:
-    #     st.title("Menu:")
-    #     pdf_docs = st.file_uploader("Upload your PDF Files and Click on the Submit & Process Button", accept_multiple_files=True)
-    #     if st.button("Submit & Process"):
-    #         with st.spinner("Processing..."):
-    #             raw_text = get_pdf_text(pdf_docs)
-    #             text_chunks = get_text_chunks(raw_text)
-    #             get_vector_store(text_chunks)
-    #             st.success("Done")
+        print(response)
+        st.write("Reply: ", response["output_text"])
 
 
 
-if __name__ == "__main__":
-    main()
+
+    def main():
+        st.header("UPSC Research Assistant")
+
+        user_question = st.text_input("Ask a Question about disaster management")
+
+        if user_question:
+            user_input(user_question)
+            
+        with st.sidebar:
+            authenticator.logout("Logout", "sidebar")
+
+        # with st.sidebar:
+        #     st.title("Menu:")
+        #     pdf_docs = st.file_uploader("Upload your PDF Files and Click on the Submit & Process Button", accept_multiple_files=True)
+        #     if st.button("Submit & Process"):
+        #         with st.spinner("Processing..."):
+        #             raw_text = get_pdf_text(pdf_docs)
+        #             text_chunks = get_text_chunks(raw_text)
+        #             get_vector_store(text_chunks)
+        #             st.success("Done")
+
+
+
+    if __name__ == "__main__":
+        main()
---
+++ generate_keys.py
@@ -0,0 +1,15 @@
+import pickle
+from pathlib import Path
+
+import streamlit_authenticator as stauth
+
+# Define user data
+names = ["Nikhil Arya", "Vaibhav Gupta"]
+usernames = ["nyxdesdev", "vaibhu"]  # Use usernames instead of usersnames
+passwords = ["abc123", "def123"]
+
+hashed_passwords = stauth.Hasher(passwords).generate()
+
+file_path = Path(__file__).parent / "hashed_pw.pkl"
+with file_path.open("wb") as file:
+    pickle.dump(hashed_passwords, file)
